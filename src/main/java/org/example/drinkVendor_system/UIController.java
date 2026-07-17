f7a52cd1cf27aa3f7679b08b318131e9669462db
hiroi_ryusei <r.hiroi.sys25@morijyobi.ac.jp>
2026-07-17 11:54:30 +0900

    お金ボタン押下処理の仮完成

--- src/main/java/org/example/drinkVendor_system/UIController.java
+++ src/main/java/org/example/drinkVendor_system/UIController.java
@@ -1,4 +1,46 @@
 package org.example.drinkVendor_system;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class UIController {
+    @FXML
+    private Button pushMoneyBtn; // fxmlファイルのonAction準拠
+//    お金ボタンが押されたときの処理
+    void pushMoneyBtn(ActionEvent e){
+//        何円のボタンが押されたのかのチェック
+        Button button = (Button) e.getSource(); // 押されたボタンの情報を取得
+        String idString = button.getId(); // fx:idを取得する
+        int InputAmount = 0;
+        switch (idString){
+//            5円の時加算なしで処理を終了する
+            case "5": break ;
+//            それ以外のボタンが押されたときは数字を受け取ってsumMoney（総投入金額）に加算する
+            case "10":
+                InputAmount = 10 ;
+                break;
+            case "50":
+                InputAmount = 50 ;
+                break;
+            case "100":
+                InputAmount = 100 ;
+                break;
+            case "500" :
+                InputAmount = 500 ;
+                break;
+            case "1000":
+                InputAmount = 1000 ;
+                break;
+            default:
+        }
+//        加算処理
+        if (InputAmount >= 5){
+//            どこからDrinkVendor内のsumMoneyを取得するか確認して変更する
+            sumMoney += InputAmount ;
+        }
+//        changeBtnColorを呼び出して表示の色を変更する
+        changeBtnColor() ;
+    }
+
 }
