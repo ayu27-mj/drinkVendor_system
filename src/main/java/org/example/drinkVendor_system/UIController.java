16b8d73a6ea469a5900bd50e3fbdec2b4c485b39
hiroi_ryusei <r.hiroi.sys25@morijyobi.ac.jp>
2026-07-21 09:43:05 +0900

    fx:idの追記

--- src/main/java/org/example/drinkVendor_system/UIController.java
+++ src/main/java/org/example/drinkVendor_system/UIController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class UIController {
-//    onAction準拠
     void pushDrinkBtn( ActionEvent e ){
 //        商品のボタンが押されたとき、何のボタンが押されたのかを取得する
         Button button = (Button) e.getSource(); // 押されたボタンの情報を取得
@@ -13,9 +12,15 @@ public class UIController {
 //        押された商品に応じてserve用の商品IDを取得する
         int prodID = 0 ;
         switch (idString){
-//            fxmlファイルを内の商品準拠、Itemに格納する内容にもよる（要変更）
-            case "coffee": prodID = 1; break;
-            case "drink": prodID = 2; break;
+            case "ColaButton": prodID = 1; break;
+            case "OrangeButton": prodID = 2; break;
+            case "AppleButton" : prodID = 3; break;
+            case "CoffeeButton" : prodID = 4; break;
+            case "CocoaButton" : prodID = 5; break;
+            case "WaterButton" : prodID = 6; break;
+            case "SparklingWaterButton" : prodID = 7; break;
+            case "GreenButton" : prodID = 8; break;
+            case "TeaButton" : prodID = 9; break;
         }
 //        商品IDをserveDrinkに送って取り出し口に商品を表示させる
         serveDrink( prodID ) ;
