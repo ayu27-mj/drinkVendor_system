2aa0328e3411ca4e2589cb118b915e09ae6cbe1d
SakuKikuchi <s.kikuchi.sys25@morijyobi.ac.jp>
2026-07-28 10:30:14 +0900

    製作完了

--- src/Test/com/example/drinkVendor_system/ItemTest.java
+++ src/Test/com/example/drinkVendor_system/ItemTest.java
@@ -1,7 +1,56 @@
 package com.example.drinkVendor_system;
 
+import org.example.drinkVendor_system.Item;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
+
+    @Test
+    void constructorTest() {
+        Item item = new Item(1, "コーラ", 150, "cola.png");
+
+        assertEquals(1, item.getId());
+        assertEquals("コーラ", item.getName());
+        assertEquals(150, item.getPrice());
+        assertEquals("cola.png", item.getImgURL());
+    }
+
+    @Test
+    void setIdTest() {
+        Item item = new Item(1, "コーラ", 150, "cola.png");
+
+        item.setId(2);
+
+        assertEquals(2, item.getId());
+    }
+
+    @Test
+    void setNameTest() {
+        Item item = new Item(1, "コーラ", 150, "cola.png");
+
+        item.setName("お茶");
+
+        assertEquals("お茶", item.getName());
+    }
+
+    @Test
+    void setPriceTest() {
+        Item item = new Item(1, "コーラ", 150, "cola.png");
+
+        item.setPrice(200);
+
+        assertEquals(200, item.getPrice());
+    }
+
+    @Test
+    void setImgURLTest() {
+        Item item = new Item(1, "コーラ", 150, "cola.png");
+
+        item.setImgURL("tea.png");
+
+        assertEquals("tea.png", item.getImgURL());
+    }
 
 }
